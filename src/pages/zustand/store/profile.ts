b0cff7ca211c0da1dfbbb4b67004842a10196bf9
Stephen <stephen@chew.es>
2024-06-26 13:45:32 +0800

    Add `devtools` to zustand.

--- src/pages/zustand/store/profile.ts
+++ src/pages/zustand/store/profile.ts
@@ -1,11 +1,14 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 type Profile = {
   name: string;
   setName: (name: string) => void;
 };
 
-export const useProfileStore = create<Profile>((set) => ({
-  name: '',
-  setName: (name) => set({ name }),
-}));
+export const useProfileStore = create<Profile, [['zustand/devtools', never]]>(
+  devtools((set) => ({
+    name: '',
+    setName: (name) => set({ name }),
+  })),
+);
